a16186a88c0035dece5dddb9e33b9391104a4880
Cipher018 <c.cueva018@gmail.com>
2026-01-05 23:11:42 -0300

    Delete APP directory

--- APP/preload.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  onTelemetryData: (callback) => ipcRenderer.on('telemetry-data', (_event, value) => callback(value)),
-  listPorts: () => ipcRenderer.invoke('list-ports'),
-  connectSerial: (portPath) => ipcRenderer.invoke('connect-serial', portPath)
-});
